400faad75639b98a36f8279889c0d96a0150b203
Prabigya Kafle <prabigyakafle50@gmail.com>
2024-02-06 23:33:28 +0545

    changes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     var jobTitleElement = document.getElementById('job-title');
     var textToType = 'A Frontend Developer.';
     var initialDelay = 800;
     var typingSpeed = 80;
 
-    
     function typeText(element, text, speed) {
         var index = 0;
         function typeNextCharacter() {
@@ -18,8 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
         element.innerHTML = '';
         setTimeout(typeNextCharacter, initialDelay);
     }
+
     typeText(jobTitleElement, textToType, typingSpeed);
-});
-document.getElementById('mobile-menu').addEventListener('click', function () {
-    document.querySelector('nav').classList.toggle('active');
+
+    document.getElementById('mobile-menu').addEventListener('click', function () {
+        document.querySelector('nav').classList.toggle('active');
+    });
 });
